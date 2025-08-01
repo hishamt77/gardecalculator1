e8e31d5c5361145b9ec0842f1b4ea56e33489165
hishamt77 <mh852129@gmail.com>
2025-08-01 11:28:59 +0530

    modified

--- index.js
+++ index.js
@@ -5,30 +5,38 @@ function calculateGrade(score)
     if (score >= 90) 
     {
         console.log("Grade: A+");
+        document.write("Grade: A+<br>");
     }
      else if (score >= 80) 
     {
         console.log("Grade: A");
+        document.write("Grade: A<br>");
     }
      else if (score >= 70) 
     {
         console.log("Grade: B+");
+        document.write("Grade: B+<br>");
     }
     else if(score>= 60)
     {
-        console.log("Grade: B")
+        console.log("Grade: B");
+        document.write("Grade: B<br>");
     }
     else
     {
-        console.log("Grade: F")
+        console.log("Grade: F");
+        document.write("Grade: F<br>");
     }
 
 }
 function displayResult(name,score)
 {
+    
+    document.write("student: " + name + "<br>");
+    document.write("Score: " + score + "<br>");
+    let grade = calculateGrade(score);
     console.log("Student: "+name)
     console.log("Score: "+score)
-    calculateGrade(score)
 }
 
 displayResult(name,score);
